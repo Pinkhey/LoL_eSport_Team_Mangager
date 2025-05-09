cfce26c09a0d1b9bcbbb88c352ebf8a2d50d766f
olvodisoma <olvodisoma@gmail.com>
2025-05-09 17:47:35 +0200

    Login Page alapok

--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LoL_eSport_Team_Manager;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,37 @@ namespace LoL_eSport_Team_Mangager
         public LoginPage()
         {
             InitializeComponent();
+        }
+
+        private void UsernameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UsernamePlaceholder.Visibility = string.IsNullOrEmpty(UsernameTextBox.Text)
+                ? Visibility.Visible
+                : Visibility.Collapsed;
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordPlaceholder.Visibility = string.IsNullOrEmpty(PasswordBox.Password)
+                ? Visibility.Visible
+                : Visibility.Collapsed;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            if (username == "admin" && password == "1234")
+            {
+                var mainWindow = (MainWindow)Application.Current.MainWindow;
+                mainWindow.MainFrame.Content = null;
+                mainWindow.LogoutButton.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                MessageBox.Show("Hibás felhasználónév vagy jelszó!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
